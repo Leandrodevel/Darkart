6d73de9a301490cecde4aec451cb9ecfabd638ac
Leandro Luiz <leandro.wdsn@gmail.com>
2026-09-26 08:15:23 -0300

    menu-modulos

--- index-modulos.js
+++ index-modulos.js
@@ -1,3 +1,4 @@
+//modulo do menu principal
 fetch('modulos/menu-index.html')
     .then(response => response.text())
     .then(html => {
@@ -6,3 +7,16 @@ fetch('modulos/menu-index.html')
     .catch(error => {
         console.error('Erro ao carregar o menu:', error);
     });
+
+    //modulo do ADS 1
+
+    fetch('modulos/ads-1.html')
+    .then(response => response.text())
+    .then(html => { 
+        document.getElementById('ads-container-1').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Erro ao carregar o anúncio 1:', error);
+    });
+
+    
