aec0bb16ab50368dcc8b0921a098aceb322be67c
Leandro Luiz <leandro.wdsn@gmail.com>
2026-09-26 09:07:19 -0300

    r

--- MEUS-MODULOS.js
+++ MEUS-MODULOS.js
@@ -1,5 +1,6 @@
-//modulo do menu principal
-fetch('modulos/menu-index.html')
+   <div id="menu-container"></div>
+<script>
+    fetch('modulos/menu-index.html')
     .then(response => response.text())
     .then(html => {
         document.getElementById('menu-container').innerHTML = html;
@@ -7,16 +8,20 @@ fetch('modulos/menu-index.html')
     .catch(error => {
         console.error('Erro ao carregar o menu:', error);
     });
+lucide.createIcons();
+</script>
 
-    //modulo do ADS 1
-
+   
+   
+   <div id="ads-container-1"></div>
+<script>
     fetch('modulos/ads-1.html')
     .then(response => response.text())
-    .then(html => { 
+    .then(html => {
         document.getElementById('ads-container-1').innerHTML = html;
     })
     .catch(error => {
-        console.error('Erro ao carregar o anúncio 1:', error);
+        console.error('Erro ao carregar o anúncio:', error);
     });
+    </script>
 
-    
